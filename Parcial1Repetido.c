47928ecc470d0b589db289b5b073551a2c5392cf
DiegoGuevara <37491492+DiegoGuevara1996@users.noreply.github.com>
2018-10-02 09:59:51 -0600

    Add files via upload

---
+++ Parcial1Repetido.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ( ) {
+	int filas, i ,j, **a ,valores,columnas;
+	
+	
+	filas=printf ("ingrese las filas de la matriz a ", "%d");
+	scanf("%d", &filas);
+	columnas=printf ("ingrese las columnas de la matriz a ", "%d");
+	scanf ("%d", &columnas);
+	for (i=0; i<filas; i++){
+		for (j=0; j<columnas; j++){
+		
+		
+		valores=printf ("ingrese los valores de la matriz a", "%d");
+		scanf("%d", &valores);
+	}
+	
+	
+}
+
+
+}
+
